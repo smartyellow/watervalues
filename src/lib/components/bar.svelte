1efd9928c69e2bb4e6acfb6cf68d34c9a3672d38
Romein van Buren <romein@vburen.nl>
2022-07-04 10:23:22 +0200

    Allow for custom measure objects to be passed
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- src/lib/components/bar.svelte
+++ src/lib/components/bar.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import { calculatePercentage, generateCssGradient } from '../css';
-  import type { LeftOrRight, WaterMeasureId } from '../types';
+  import type { LeftOrRight, WaterMeasure, WaterMeasureId } from '../types';
   import measures from '../measures';
   import './bar.scss';
 
   /** The water measure id. */
   export let measureId: WaterMeasureId | string;
+
+  /**
+   * Water measure object
+   *
+   * Can be omitted if measureId is set.
+   */
+  export let measure: WaterMeasure | undefined = measureId ? measures[measureId] : undefined;
 
   /** The current water value to display. */
   export let value: number | undefined = undefined;
@@ -15,6 +22,10 @@
 
   /** Show a marker on the bar that indicates the value. */
   export let showMarker = true;
+
+  if (!measureId && !measure) console.warn(
+    'Water value bar: at least one of `measureId` and `measure` must be set.'
+  );
 </script>
 
 <div class="watervaluebar">
